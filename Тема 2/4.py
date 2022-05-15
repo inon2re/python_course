bf23ec4bf9cf5ea836ddfc28d3d06b2895c35fee
inon2re <hulumulunulu@yandex.ru>
2022-05-15 13:19:32 +0300

    add file 2.4.py and refact file 2.3.py

---
+++ Тема 2/4.py
@@ -0,0 +1,6 @@
+FirstSide = int(input('Введите 1 сторону '))
+SecondSide = int(input('Введите 2 сторону '))
+if FirstSide == SecondSide:
+    print('Четырехугольник - квадрат')
+else:
+    print('Четырехугольник не квадрат')
