e1a6db753a8f2a3bdd3373ec173de8f908318879
inon2re <hulumulunulu@yandex.ru>
2022-07-06 11:17:49 +0300

    Add file 7.3.py

---
+++ Тема 7/3.py
@@ -0,0 +1,20 @@
+with open("Texts/7-1.txt", encoding='utf-8') as stih:
+    lstwords = []
+    lstwords2 = []
+    count = 0
+    word = ''
+    for _ in stih:
+        lstwords.append(_.split())
+    for i in lstwords:
+        for a in i:
+            lstwords2.append(a)
+    for index in range(len(lstwords2)):
+        if count < len(lstwords2[index]):
+            count = len(lstwords2[index])
+            word = lstwords2[index]
+    lstwords.clear()
+    lstwords.append(word)
+    for words in lstwords2:
+        if count == len(words) and not words == word:
+            lstwords.append(words)
+print(lstwords)
