e5cd96e6e8984ef1fec46a28e12fe0db6af04550
inon2re <hulumulunulu@yandex.ru>
2022-08-14 12:08:46 +0300

    Final project ))

--- Итоговый проект/1.py
+++ Итоговый проект/1.py
@@ -1,14 +1,6 @@
-# import requests
-# # https://stopgame.ru/news
-# #
-# out = requests.get("https://images.stopgame.ru/news/2022/07/23/c270x153/IQ_xVC4sjq-srCUPKB9gTQ/_yFN7XWm.jpg")
-# # print(out.content)
-# with open('new_images.png', 'wb') as f:
-#     f.write(out.content)
 
 import requests
 from bs4 import BeautifulSoup
-import os
 
 url = 'https://stopgame.ru/news'
 response = requests.get(url)
@@ -21,7 +13,6 @@ for image in images:
     src = image.get("src")
     if src:
         UrlList.append(src)
-# print(UrlList)
 
 for i in range(len(UrlList)-1):
     out = requests.get(UrlList[i])
